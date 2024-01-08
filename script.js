6e25a7e224d87374f93a3f45e0bcc68f2ffa4f1f
Danilopc <sr_danilo@hotmail.com>
2024-01-08 19:38:37 +0000

    create functions+ get IDs + localstorage

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-var currentDayEl = $('#currentDay')
+var currentDayEl = $('#currentDay');
 var container = $(".container");
 
-//date display
-var now = dayjs('05-01-2024').format('dddd' +', '+ 'MMMM MM')
+// Date display
+var now = dayjs('05-01-2024').format('dddd' + ', ' + 'MMMM DD');
 currentDayEl.text(now);
+var currentHour = dayjs().format('HH');
 
-//color scheme
-var currentHour =dayjs().format('HH')
+//create timeblocks using a function and id
+for (var i = 9; i <= 22; i++) {
+    createRow(i);
+}
 
-// create timeblocks
-    for (var i = 9; i <= 20; i++) {
-      var row = $("<div>").addClass("row");
-      var hourDiv = $("<div>").addClass("col-1 hour time-block").text(i + ":00 pm");
-      var textarea = $("<textarea>").attr("id", i).addClass("col-8 input textarea");
-      var saveBtn = $("<button>").addClass("col-1 saveBtn").html('<i class="far fa-save"></i>');
-     
-        //adding colors
-        if(Number(currentHour) == textarea.attr('id')){
-            textarea.addClass("present")
-        } else if(Number(currentHour) < textarea.attr('id')){
-            textarea.addClass("future")
-        } else{
-            textarea.addClass("past")
-        }
-        console.log(textarea.attr("id"))
+// Function to create a row with ID
+function createRow(id) {
+    //get the values from localStorage before create rows
+    var storedValue = localStorage.getItem(id);
 
-      row.append(hourDiv, textarea, saveBtn);
-      container.append(row);
+    var row = $("<div>").addClass("row");
+    var hourDiv = $("<div>").addClass("col-1 hour time-block").text(id + ":00 pm");
+    var textarea = $("<textarea>").attr("id", id).addClass("col-8 input textarea").val(storedValue);
+    var saveBtn = $("<button>").addClass("col-1 saveBtn").html('<i class="far fa-save"></i>');
+
+    // Adding colors
+    if (Number(currentHour) == id) {
+        textarea.addClass("present");
+    } else if (Number(currentHour) < id) {
+        textarea.addClass("future");
+    } else {
+        textarea.addClass("past");
     }
+    // Save button click event
+    saveBtn.on('click', function () {
+        saveLocalStorage(textarea);
+    });
 
+    row.append(hourDiv, textarea, saveBtn);
+    container.append(row);
+}
+
+// Function save local storage
+function saveLocalStorage(textarea) {
+    var textareaId = textarea.attr('id');
+    var textareaValue = textarea.val();
+
+    localStorage.setItem(textareaId, textareaValue);
+}
+
+// save buttons event
+container.on('click', '.saveBtn', function () {
+    var textarea = $(this).prev();
+    saveLocalStorage(textarea);
+});
